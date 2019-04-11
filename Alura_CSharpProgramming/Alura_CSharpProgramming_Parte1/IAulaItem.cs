d8b7ffcab8606266947e41f1498d036b6d1bb266
guilherme_bmq <guilhermebentomarques@gmail.com>
2019-04-11 09:19:47 -0300

    Inclusão Aula 1

---
+++ Alura_CSharpProgramming/Alura_CSharpProgramming_Parte1/IAulaItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura_CSharpProgramming_Parte1
+{
+    interface IAulaItem
+    {
+        void Executar();
+    }
+}
