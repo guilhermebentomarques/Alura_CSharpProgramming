80094627511c23fe7106d06bb66cfdda4c074a7e
Guilherme Marques <guilherme.marques@quartalfs.com>
2020-01-31 15:37:49 +0000

    Include Array and Generics Project

---
+++ Alura_CSharpProgramming/Alura_CSharp_ArraysAndGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alura_CSharp_ArraysAndGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
